5c396d52c36784316937ffcad529884c07041002
sechiller <103475613+sechiller@users.noreply.github.com>
2022-05-28 19:00:52 -0500

    Add files via upload
    Deliverable 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,26 @@
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
